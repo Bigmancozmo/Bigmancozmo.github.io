036eb044878de6e9636c00a96128c045ac333957
mcnedj468 <cozmod275@gmail.com>
2021-05-07 15:44:03 -0400

    Add "text.css"

--- codehelper/editor/saveload.js
+++ codehelper/editor/saveload.js
@@ -4,6 +4,6 @@ function save(){
 	let myprojects = fetch("myprojects.json")
 	.then(response => response.json())
 	.then(myprojects => {
-		console.log(myprojects.projects[myprojectnumber - 1]);
+		window.localStorage.setItem("savefile_codehelper_bmc",myprojects.toString());
 	});
 }
